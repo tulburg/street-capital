85a4bb6894da392d6c11d36c9077a6481630270d
jeezman <tobiadeyemi91@gmail.com>
2018-04-13 11:28:17 +0100

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Login from './auth/Login';
-import ForgotPassword from './auth/ForgotPassword';
-import ResetPassword from './auth/ResetPassword';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Login from "./auth/Login";
+import ForgotPassword from "./auth/ForgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <ResetPassword/>
+      <Router>
+        <div>
+          <Route exact path="/" component={Login} />
+          <Route path="/reset-password" component={ResetPassword} />
+          <Route path="/forgot-password" component={ForgotPassword} />
+        </div>
+      </Router>
     );
   }
 }
 
 const Container = styled.div`
- border: 1px solid blue;
- display: flex;
- height: 100vh;
-`
+  border: 1px solid blue;
+  display: flex;
+  height: 100vh;
+`;
 
 export default App;
