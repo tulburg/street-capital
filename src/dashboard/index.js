b62cd2568bf344207d304a0c6560e1b9bc5fb0b7
Jeezman <tobiadeyemi91@gmail.com>
2018-04-17 04:08:16 +0100

    Update Dashboard to handle nested routes
    - Add Modal component to dashboard

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,7 +1,107 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Modal from "../components/Modal";
 
+const SendInvite = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 48px;
+    font-weight: bold;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: left;
+    color: #9b9b9b;
+    margin-bottom: 20px;
+  }
+
+  h4 {
+    font-size: 18px;
+    font-weight: bold;
+    letter-spacing: 1.3px;
+    color: #000000;
+    margin-bottom: 20px;
+
+    &.uppercase {
+      text-transform: uppercase;
+    }
+  }
+
+  .subtext {
+    opacity: 0.59;
+    font-size: 14px;
+    color: #4a4a4a;
+  }
+
+  .textbox {
+    width: 456px;
+    height: 63px;
+    border-radius: 5px;
+    background-color: #f5f6fa;
+    border: solid 1px #979797;
+    font-size: 18px;
+    color: #9b9b9b;
+    padding-left: 15px;
+    margin-top: 25px;
+    margin-bottom: 35px;
+  }
+
+  button {
+    width: 143px;
+    height: 55px;
+    border-radius: 9px;
+    background-color: #50e3c2;
+    border: 0;
+    font-size: 18px;
+    font-weight: bold;
+    color: #1c2d41;
+  }
+
+  .input-wrap {
+    position: relative;
+
+    &__btn {
+      width: 40px;
+      height: 40px;
+      border-radius: 100%;
+      border: 0;
+      position: absolute;
+      right: 12px;
+      top: 38px;
+      background-color: #50e3c2;
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .invite-sent {
+    font-size: 14px;
+    color: #000000;
+  }
+`;
 export default class Dashboard extends Component {
+  state = {
+    showModal: true,
+    showDoneModal: false
+  };
+
+  onCloseModal = () => {
+    this.setState({
+      showModal: false,
+      showDoneModal: false
+    });
+  };
   render() {
     return (
       <div>
@@ -15,138 +115,206 @@ export default class Dashboard extends Component {
           <div className="navigation">
             <NavigationCard>
               <li className="navigation__item">
-                <a href="/" className="navigation__link">
+                <Link
+                  to={`${this.props.match.url}`}
+                  className="navigation__link"
+                >
                   <span className="navigation__link-icon">
                     <i className="fas fa-home" />
                   </span>
                   <span className="navigation__link-text">Home</span>
-                </a>
+                </Link>
               </li>
               <li className="navigation__item">
-                <a href="/" className="navigation__link">
+                <Link
+                  to={`${this.props.match.url}/tests`}
+                  className="navigation__link"
+                >
                   <span className="navigation__link-icon">
                     <i className="far fa-file-alt" />
                   </span>
                   <span className="navigation__link-text">Tests</span>
-                </a>
+                </Link>
               </li>
               <li className="navigation__item">
-                <a href="/" className="navigation__link">
+                <Link
+                  to={`${this.props.match.url}/flashcards`}
+                  className="navigation__link"
+                >
                   <span className="navigation__link-icon">
                     <i className="fas fa-sticky-note" />
                   </span>
                   <span className="navigation__link-text">Flashcards</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <hr />
               </li>
               <li className="navigation__item">
-                <a href="/" className="navigation__link">
+                <Link
+                  to={`${this.props.match.url}/payments`}
+                  className="navigation__link"
+                >
                   <span className="navigation__link-icon">
                     <i className="far fa-credit-card" />
                   </span>
                   <span className="navigation__link-text">Payments</span>
-                </a>
+                </Link>
               </li>
               <li className="navigation__item">
-                <a href="/" className="navigation__link">
+                <Link
+                  to={`${this.props.match.url}/users`}
+                  className="navigation__link"
+                >
                   <span className="navigation__link-icon">
                     <i className="fas fa-user" />
                   </span>
                   <span className="navigation__link-text">All Users</span>
-                </a>
+                </Link>
               </li>
             </NavigationCard>
           </div>
 
-          <div className="main-section">
-            <h2 className="main-section__header">Overview</h2>
-            <div className="card-wrap">
-              <Card>
-                <div className="top">
-                  <div className="icon">
-                    <i className="far fa-address-card" />
-                  </div>
-                  <div className="card__info">
-                    <h2>3600</h2>
-                    <p>Signed Up Users</p>
-                  </div>
+          <Route
+            path={`${this.props.match.url}`}
+            render={() => (
+              <div className="main-section">
+                <Modal
+                  closeModal={this.onCloseModal}
+                  visible={this.state.showModal}
+                >
+                  <SendInvite>
+                    <h4 className="header">Invite users</h4>
+                    <span className="subtext">
+                      Enter an Email Address to Continue
+                    </span>
+                    <input className="textbox" placeholder="Email Address" />
+                    <button
+                      onClick={() =>
+                        this.setState({ showDoneModal: true, showModal: false })
+                      }
+                    >
+                      Invite
+                    </button>
+                  </SendInvite>
+                </Modal>
+                <Modal
+                  closeModal={this.onCloseModal}
+                  visible={this.state.showDoneModal}
+                >
+                  <SendInvite>
+                    <h1 className="header--large">Done!</h1>
+                    <h4 className="header uppercase">
+                      Invite more users to use lawyr
+                    </h4>
+                    <span className="subtext">
+                      Enter another Email Address to Continue
+                    </span>
+                    <div className="input-wrap">
+                      <input className="textbox" placeholder="Email Address" />
+                      <div type="button" className="input-wrap__btn">
+                        <i className="fas fa-plus" />
+                      </div>
+                    </div>
+                    <span className="invite-sent">
+                      Invitation Email to user@devcenter.co sent{" "}
+                    </span>
+                  </SendInvite>
+                </Modal>
+                <h2 className="main-section__header">Overview</h2>
+                <div className="card-wrap">
+                  <Card>
+                    <div className="top">
+                      <div className="icon">
+                        <i className="far fa-address-card" />
+                      </div>
+                      <div className="card__info">
+                        <h2>3600</h2>
+                        <p>Signed Up Users</p>
+                      </div>
+                    </div>
+                    <button onClick={() => this.setState({ showModal: true })}>
+                      Invite Users
+                    </button>
+                  </Card>
+                  <Card className="card--white">
+                    <div className="top">
+                      <div className="icon">
+                        <i className="fas fa-users" />
+                      </div>
+                      <div className="card__info">
+                        <h2>400</h2>
+                        <p>Active Users</p>
+                      </div>
+                    </div>
+                    <button onClick={() => this.setState({ showModal: true })}>
+                      Invite Users
+                    </button>
+                  </Card>
+                  <Card className="card--white">
+                    <div className="top">
+                      <div className="icon">
+                        <i className="far fa-file-alt" />
+                      </div>
+                      <div className="card__info">
+                        <h2>1000</h2>
+                        <p>Paid Users</p>
+                      </div>
+                    </div>
+                    <button onClick={() => this.setState({ showModal: true })}>
+                      Invite Users
+                    </button>
+                  </Card>
                 </div>
-                <button>Invite Users</button>
-              </Card>
-              <Card className="card--white">
-                <div className="top">
-                  <div className="icon">
-                    <i className="fas fa-users" />
-                  </div>
-                  <div className="card__info">
-                    <h2>400</h2>
-                    <p>Active Users</p>
-                  </div>
-                </div>
-                <button>Invite Users</button>
-              </Card>
-              <Card className="card--white">
-                <div className="top">
-                  <div className="icon">
-                    <i className="far fa-file-alt" />
-                  </div>
-                  <div className="card__info">
-                    <h2>1000</h2>
-                    <p>Paid Users</p>
-                  </div>
-                </div>
-                <button>Invite Users</button>
-              </Card>
-            </div>
 
-            <div className="main-section__separator" />
+                <div className="main-section__separator" />
 
-            <h2 className="main-section__header">Content</h2>
-            <div className="card-wrap">
-              <FlashCard>
-                <p className="flashcard__text">Create a Flash card</p>
-                <span className="flashcard__icon">
-                  <i className="fas fa-arrow-right fa-lg" />
-                </span>
-              </FlashCard>
-              <FlashCard>
-                <p className="flashcard__text">Create a Test</p>
-                <span className="flashcard__icon">
-                  <i className="fas fa-arrow-right fa-lg" />
-                </span>
-              </FlashCard>
-              <div className="quick-links">
-                <p className="quick-links__header">quick links</p>
-                <p className="quick-links__link">
-                  <span>Edit Flashcards</span>
-                  <span>
-                    <i className="fas fa-arrow-right" />
-                  </span>
-                </p>
-                <p className="quick-links__link">
-                  <span>Edit Courses</span>
-                  <span>
-                    <i className="fas fa-arrow-right" />
-                  </span>
-                </p>
+                <h2 className="main-section__header">Content</h2>
+                <div className="card-wrap">
+                  <FlashCard>
+                    <p className="flashcard__text">Create a Flash card</p>
+                    <span className="flashcard__icon">
+                      <i className="fas fa-arrow-right fa-lg" />
+                    </span>
+                  </FlashCard>
+                  <FlashCard>
+                    <p className="flashcard__text">Create a Test</p>
+                    <span className="flashcard__icon">
+                      <i className="fas fa-arrow-right fa-lg" />
+                    </span>
+                  </FlashCard>
+                  <div className="quick-links">
+                    <p className="quick-links__header">quick links</p>
+                    <p className="quick-links__link">
+                      <span>Edit Flashcards</span>
+                      <span>
+                        <i className="fas fa-arrow-right" />
+                      </span>
+                    </p>
+                    <p className="quick-links__link">
+                      <span>Edit Courses</span>
+                      <span>
+                        <i className="fas fa-arrow-right" />
+                      </span>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="main-section__separator" />
+
+                <div className="invite-users">
+                  <h2 className="invite-users__header">Invite users</h2>
+                  <div className="invite-users__input-wrap">
+                    <input />
+                    <button>invite</button>
+                  </div>
+                  <p className="invite-users__link">
+                    <a href="/">Go to users page</a>
+                  </p>
+                </div>
               </div>
-            </div>
-
-            <div className="main-section__separator" />
-
-            <div className="invite-users">
-              <h2 className="invite-users__header">Invite users</h2>
-              <div className="invite-users__input-wrap">
-                <input />
-                <button>invite</button>
-              </div>
-              <p className="invite-users__link">
-                <a href="/">Go to users page</a>
-              </p>
-            </div>
-          </div>
+            )}
+          />
         </Content>
       </div>
     );
