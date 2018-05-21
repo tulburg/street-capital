0cea70908dbad576327de1a2e5a4d83a4d36676e
Jeezman <tobiadeyemi91@gmail.com>
2018-05-21 04:01:11 +0100

    Add CourseIcon component

---
+++ src/components/CourseIcon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+/**
+ *
+ * @param {string} course_name name/title of course
+ */
+const CourseIcon = ({ course_title }) => (
+  <CourseIconStyle>
+    <div className="icon" />
+    <p className="title">{course_title}</p>
+  </CourseIconStyle>
+);
+
+const CourseIconStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .icon {
+    height: 153px;
+    width: 153px;
+    border-radius: 100px;
+    background-color: #1c2d41;
+    margin-bottom: 23px;
+  }
+  .title {
+    font-weight: bold;
+  }
+`;
+
+export default CourseIcon;
